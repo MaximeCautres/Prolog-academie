d97137e76ddae4e6365a5b0eb10856b5c785e6b1
Radion <didier.zanello@epita.fr>
2020-06-01 00:46:19 +0200

    Refacto pion.py

--- chess/pion.py
+++ chess/pion.py
@@ -7,7 +7,6 @@ class Pion:
     # On renvoie les positions possibles avec ou sans manger pour un pion
     def deplacement(self, y, x, partie):
         carte = partie.map
-
         mouvement = []
         manger = []
         roque = []
@@ -33,6 +32,5 @@ class Pion:
             if y == (4 if self.couleur == 'blanc' else 3) and 0 <= x + e < 8 and carte[y + times][x + e] == 0:
                 if carte[y][x + e] != 0 and carte[y][x + e].couleur != self.couleur and carte[y][x + e].piecetype == 'pion' and carte[y][x + e].bouge_2 == partie.numero_tour - 1:
                     passant += [[y + times, x + e]]
-
 
         return mouvement, manger, roque, passant
