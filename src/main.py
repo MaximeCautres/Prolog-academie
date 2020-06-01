a4beef27607cba8de30d0152e7fca2a39399cd52
Radion <didier.zanello@epita.fr>
2020-06-01 12:42:40 +0200

    Clean code

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from copy import deepcopy
 
-from classes.knight import knight
 from classes.bishop import bishop
+from classes.king import king
+from classes.knight import knight
 from classes.pawn import pawn
 from classes.queen import queen
-from classes.king import king
 from classes.rook import rook
 
 from GUI.plateau import plateau
