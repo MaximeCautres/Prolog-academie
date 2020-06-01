2f7cec96af8a8b2dc180e875c3de127b48ca8512
Radion <didier.zanello@epita.fr>
2020-06-01 12:08:15 +0200

    Add classes as import

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
 from copy import deepcopy
 
+from classe.cavalier import cavalier
+from classe.fou import fou
+from classe.pion import pion
+from classe.reine import reine
+from classe.roi import roi
+from classe.tour import tour
+
+from GUI.plateau import plateau
+
 unit = 100
 
 if __name__ == "__main__":
