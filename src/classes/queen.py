41eb23f08a09c46b768af83a8e67e082f6646faa
Radion <didier.zanello@epita.fr>
2020-06-01 13:13:21 +0200

    Update the function

--- src/classes/queen.py
+++ src/classes/queen.py
@@ -1,23 +1,23 @@
-class Renne:
-    def __init__(self, couleur):
-        self.couleur = couleur
-        self.piecetype = 'renne'
+class Queen:
+    def __init__(self, color):
+        self.colors = color
+        self.piecetype = 'queen'
 
-    # On retourne les position ou peu se déplacer la tour avec ou sans manger
-    def deplacement(self, y, x, partie):
-        carte = partie.map
-        manger = []
-        roque = []
-        mouvement = []
+    # Return position where can move
+    def move(self, yPos, xPos, game):
+        mapGame = game.map
+        eat = []
+        castling = []
+        movement = []
 
-        cas = [[1, 1], [-1, 1], [-1, -1], [1, -1], [1, 0], [0, 1], [-1, 0], [0, -1]]
+        cases = [[1, 1], [-1, 1], [-1, -1], [1, -1], [1, 0], [0, 1], [-1, 0], [0, -1]]
 
-        for e1, e2 in cas:
+        for e1, e2 in cases:
             di = [[y, x]]
-            while 0 <= di[-1][0] + e1 < 8 and 0 <= di[-1][1] + e2 < 8 and carte[di[-1][0] + e1][di[-1][1] + e2] == 0:
+            while 0 <= di[-1][0] + e1 < 8 and 0 <= di[-1][1] + e2 < 8 and mapGame[di[-1][0] + e1][di[-1][1] + e2] == 0:
                 di += [[di[-1][0] + e1, di[-1][1] + e2]]
-            if 0 <= di[-1][0] + e1 < 8 and 0 <= di[-1][1] + e2 < 8and carte[di[-1][0] + e1][di[-1][1] + e2].couleur != self.couleur:
-                manger += [[di[-1][0] + e1, di[-1][1] + e2]]
-            mouvement += di[1:]
+            if 0 <= di[-1][0] + e1 < 8 and 0 <= di[-1][1] + e2 < 8 and mapGame[di[-1][0] + e1][di[-1][1] + e2].color != self.color:
+                eat += [[di[-1][0] + e1, di[-1][1] + e2]]
+            movement += di[1:]
 
-        return mouvement, manger, roque, []
+        return movement, eat, castling, []
