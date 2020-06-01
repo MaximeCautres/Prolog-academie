7b48f50d5d145706599d274ab69b0af6fc61c509
Radion <didier.zanello@epita.fr>
2020-06-01 12:54:20 +0200

    Update the function with names

--- src/classes/king.py
+++ src/classes/king.py
@@ -1,22 +1,23 @@
-class Roi:
-    def __init__(self, couleur):
-        self.abouge = False
-        self.couleur = couleur
-        self.piecetype = 'roi'
+class King:
+    def __init__(self, color):
+        self.moved = False
+        self.color = color
+        self.piecetype = 'king'
 
     # On retourne la position du roi avec ou sans manger
-    def deplacement(self, y, x, parite):
-        carte = partie.map
-        manger = []
-        mouvement = []
-        roque = []
+    # Return the king position with or without eating
+    def move(self, y, x, game):
+        mapGame = game.map
+        eat = []
+        movement = []
+        castling = []
 
-        cas = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
+        cases = [[1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1]]
 
         for e1, e2 in cas:
-            if 0 <= y + e1 < 8 and 0 <= x + e2 < 8 and carte[y + e1][x + e2] == 0:
-                mouvement += [[y + e1, x + e2]]
-            elif 0 <= y + e1 < 8 and 0 <= x + e2 < 8 and carte[y + e1][x + e2].couleur != self.couleur:
-                manger += [[y + e1, x + e2]]
+            if 0 <= y + e1 < 8 and 0 <= x + e2 < 8 and mapGame[y + e1][x + e2] == 0:
+                movement += [[y + e1, x + e2]]
+            elif 0 <= y + e1 < 8 and 0 <= x + e2 < 8 and mapGame[y + e1][x + e2].color != self.color:
+                eat += [[y + e1, x + e2]]
 
-        return mouvement, manger, roque, []
+        return movement, eat, castling, []
