4eeb0ee08902e517c393e7fcce561971d297d708
MaximeCautres <maxime.cautres@gmail.com>
2020-06-01 13:40:51 +0200

    pawn.py

--- src/classes/pawn.py
+++ src/classes/pawn.py
@@ -15,23 +15,23 @@ class Pawn:
         times = 1 if self.color == 'white' else -1
 
         # Movement
-        if 0 <= y + times < 8 and carte[y + times][x] == 0:
-                movement += [[y + times, x]]
-                if y == (1 if self.color == 'white' else 6) and mapGame[y + times * 2][x] == 0:
-                    movement += [[y + times * 2, x]]
+        if 0 <= yPos + times < 8 and carte[yPos + times][xPos] == 0:
+                movement += [[yPos + times, xPos]]
+                if yPos == (1 if self.color == 'white' else 6) and mapGame[yPos + times * 2][xPos] == 0:
+                    movement += [[yPos + times * 2, xPos]]
 
         cases = [1, -1]
 
         # Classic eating
         for e in cases:
-            if 0 <= y + times < 8 and 0 <= x + e < 8 and mapGame[y + times][x + e] != 0:
-                if mapGame[y + times][x + e].color != self.color:
-                    eat += [[y + times, x + e]]
+            if 0 <= yPos + times < 8 and 0 <= xPos + e < 8 and mapGame[yPos + times][xPos + e] != 0:
+                if mapGame[yPos + times][xPos + e].color != self.color:
+                    eat += [[yPos + times, xPos + e]]
 
         # "en passant"
         for e in cases:
-            if y == (4 if self.color == 'white' else 3) and 0 <= x + e < 8 and mapGame[y + times][x + e] == 0:
-                if mapGame[y][x + e] != 0 and mapGame[y][x + e].color != self.color and mapGame[y][x + e].piecetype == 'pown' and mapGame[y][x + e].moved == game.numero_tour - 1:
-                    enPassant += [[y + times, x + e]]
+            if yPos == (4 if self.color == 'white' else 3) and 0 <= xPos + e < 8 and mapGame[yPos + times][xPos + e] == 0:
+                if mapGame[yPos][xPos + e] != 0 and mapGame[yPos][xPos + e].color != self.color and mapGame[yPos][xPos + e].piecetyPospe == 'pown' and mapGame[yPos][xPos + e].moved == game.numero_tour - 1:
+                    enPassant += [[yPos + times, xPos + e]]
 
         return movement, eat, castling, enPassant
