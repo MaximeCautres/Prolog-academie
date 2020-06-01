36877eb475a93319de7b9c96856a4ae48e80c5d0
MaximeCautres <maxime.cautres@gmail.com>
2020-06-01 13:29:51 +0200

    bishop.py

--- src/classes/rook.py
+++ src/classes/rook.py
@@ -5,7 +5,7 @@ class Rook:
         self.piecetype = 'tour'
 
     # On retourne les position ou peu se déplacer la tour avec ou sans manger
-    def move(self, y, x, game):
+    def move(self, yPos, xPos, game):
         mapGame = game.map
         eat = []
         castling = []
@@ -14,7 +14,7 @@ class Rook:
         cases = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
         for e1, e2 in cases:
-            di = [[y, x]]
+            di = [[yPos, xPos]]
             while 0 <= di[-1][0] + e1 < 8 and 0 <= di[-1][1] + e2 < 8 and mapGame[di[-1][0] + e1][di[-1][1] + e2] == 0:
                 di += [[di[-1][0] + e1, di[-1][1] + e2]]
             if 0 <= di[-1][0] + e1 < 8 and 0 <= di[-1][1] + e2 < 8 and mapGame[di[-1][0] + e1][di[-1][1] + e2].color != self.color:
