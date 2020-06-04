9d060bcda11c54220c33c4268f927905f56cb14f
MaximeCautres <maxime.cautres@gmail.com>
2020-06-04 23:44:18 +0200

    integration of castling from the king

--- src/classes/king.py
+++ src/classes/king.py
@@ -21,5 +21,27 @@ class King(Piece):
                 movement += [[yPos + y, xPos + x]]
             elif 0 <= yPos + y < 8 and 0 <= xPos + x < 8 and mapGame[yPos + y][xPos + x].color != self.color:
                 eat += [[yPos + y, xPos + x]]
+        
+        def is_check(j, i):
 
+            check_confirmation = False
+                  
+            for y, x in game.pieces[not self.color]:
+                _, eat, _, _ = mapGame[y][x].getMove(game, True)
+                if [j, i] in eat:
+                    check_confirmation = True
+                    break
+            return check_confirmation
+
+        # small castling (right one)
+        
+        if not check and not self.moved and mapGame[yPos][7] != None and mapGame[yPos][7].piecetype == 'rook' and not mapGame[yPos][7].moved and mapGame[yPos][7].color == self.color and mapGame[yPos][5] == None and mapGame[yPos][6] == None and not is_check(yPos, 5) and not is_check(yPos, 6):
+            castling += [[yPos, 6]]
+
+        # big casling (left one)
+
+        if not check and not self.moved and mapGame[yPos][0] != None and mapGame[yPos][0].piecetype == 'rook' and not mapGame[yPos][0].moved and mapGame[yPos][0].color == self.color and mapGame[yPos][1] == None and mapGame[yPos][2] == None and mapGame[yPos][3] == None and not is_check(yPos, 2) and not is_check(yPos, 3):
+            castling += [[yPos, 2]]
+        
+        
         return movement, eat, castling, []
