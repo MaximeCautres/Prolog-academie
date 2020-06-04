2c86cfd352267992f4d41179ee6b7e6b28b0fd2a
MaximeCautres <maxime.cautres@gmail.com>
2020-06-04 16:29:39 +0200

    bug fixe

--- src/GUI/board.py
+++ src/GUI/board.py
@@ -6,8 +6,15 @@ import matplotlib.image as mpimg
 
 from pygame.locals import *
 
+from classes.bishop import Bishop
+from classes.king import King
+from classes.knight import Knight
+from classes.pawn import Pawn
+from classes.queen import Queen
+from classes.rook import Rook
+
 class Board:
-    def __init__(self, unit)):
+    def __init__(self, unit):
         # Create the table which represents the board
         self.map = [[None for _ in range(8)] for _ in range(8)]
 
@@ -46,80 +53,96 @@ class Board:
         self.king = {True: [0, 4], False: [7, 4]}
         self.unit = unit
 
-        def display(self):
-            # Creation of the board's visual
-            bakground = np.zeros((unit*8, unit*8, 3), dtype=np.uint8)
-            for i in range(8):
-                for j in range(8):
-                    if (i + j) % 2 == 0:
-                        background[i*unit:(i+1)*unit, j*unit:(j+1)*unit] = np.array([240, 209, 136])
-                    else:
-                        background[i*unit:(i+1)*unit, j*unit:(j+1)*unit] = np.array([102, 51, 0])
+    def display(self):
 
-            mpimg.imsave("background.png", background)
+        # the window is created
 
-            # Creation of the board's visual annexes
-            mpimg.imsave("whiteCase.png", np.array([[[240, 209, 136] for _ in range(unit)] for _ in range(unit)], dtype=np.uint8))
-            mpimg.imsave("blackCase.png", np.array([[[102, 51, 0] for _ in range(unit)] for _ in range(unit)], dtype=np.uint8))
-            mpimg.imsave("movementCase.png", np.array([[[128, 109, 90, 150] for _ in range(unit)] for _ in range(unit)], dtype=np.uint8))
-            mpimg.imsave("eatCase.png", np.array([[[165, 38, 10, 100] for _ in range(unit)] for _ in range(unit)], dtype=np.uint8))
-            mpimg.imsave("selectCase.png", np.array([[[1, 215, 88, 80] for _ in range(unit)] for _ in range(unit)], dtype=np.uint8))
-            mpimg.imsave("castlingCase.png", np.array([[[255, 215, 0, 60] for _ in range(unit)] for _ in range(unit)], dtype=np.uint8))
+        self.window = pygame.display.set_mode((self.unit*8, self.unit*8))
+        
+        # Creation of the board's visual
+        background = np.zeros((self.unit*8, self.unit*8, 3), dtype=np.uint8)
+        for i in range(8):
+            for j in range(8):
+                if (i + j) % 2 == 0:
+                    background[i*self.unit:(i+1)*self.unit, j*self.unit:(j+1)*self.unit] = np.array([240, 209, 136])
+                else:
+                    background[i*self.unit:(i+1)*self.unit, j*self.unit:(j+1)*self.unit] = np.array([102, 51, 0])
 
-            # We load annexes and background for futur uses
-            self.background = pygame.image.load("background.png").convert()
-            self.whiteCase = pygame.image.load("whiteCase.png").convert()
-            self.blackCase = pygame.image.load("blackCase.png").convert()
-            self.movementCase = pygame.image.load("movementCase.png").convert()
-            self.eatCase = pygame.image.load("eatCase.png").convert()
-            self.selectCase = pygame.image.load("selectCase.png").convert()
-            self.castlingCase = pygame.image.load("castlingCase.png").convert()
+        mpimg.imsave("background.png", background)
 
-            self.pieces_skin = {
-                'pawn': {True: pygame.transform.scale(pygame.image.load("../pictures/whitePawn.png").convert_alpha(),(int(unit * 0.8), int(unit * 0.8) )), False: pygame.transform.scale(pygame.image.load("../pictures/blackPawn.png").convert_alpha(), (int(unit * 0.8), int(unit * 0.8) ))},
-                'rook': {True: pygame.transform.scale(pygame.image.load("../pictures/whiteRook.png").convert_alpha(), (int(unit * 0.8), int(unit * 0.8) )), False: pygame.transform.scale(pygame.image.load("../pictures/blackRook.png").convert_alpha(), (int(unit * 0.8), int(unit * 0.8) ))},
-                'knight': {True: pygame.transform.scale(pygame.image.load("../pictures/whiteKnight.png").convert_alpha(), (int(unit * 0.8), int(unit * 0.8) )), False: pygame.transform.scale(pygame.image.load("../pictures/blackKnight.png").convert_alpha(), (int(unit * 0.8), int(unit * 0.8) ))}
-                'bishop': {True: pygame.transform.scale(pygame.image.load("../pictures/whiteBishop.png").convert_alpha(), (int(unit * 0.8), int(unit * 0.8) )), False: pygame.transform.scale(pygame.image.load("../pictures/blackBishop.png").convert_alpha(), (int(unit * 0.8), int(unit * 0.8) ))},
-                'king': {True: pygame.transform.scale(pygame.image.load("../pictures/whiteKing.png").convert_alpha(), (int(unit * 0.8), int(unit * 0.8) )), False: pygame.transform.scale(pygame.image.load("../pictures/blackKing.png").convert_alpha(), (int(unit * 0.8), int(unit * 0.8) ))},
-                'queen': {True: pygame.transform.scale(pygame.image.load("../pictures/whiteQueen.png").convert_alpha(), (int(unit * 0.8), int(unit * 0.8) )), False: pygame.transform.scale(pygame.image.load("../pictures/blackQueen.png").convert_alpha(), (int(unit * 0.8), int(unit * 0.8) ))}}
+        # Creation of the board's visual annexes
+        mpimg.imsave("whiteCase.png", np.array([[[240, 209, 136] for _ in range(self.unit)] for _ in range(self.unit)], dtype=np.uint8))
+        mpimg.imsave("blackCase.png", np.array([[[102, 51, 0] for _ in range(self.unit)] for _ in range(self.unit)], dtype=np.uint8))
+        mpimg.imsave("movementCase.png", np.array([[[128, 109, 90, 150] for _ in range(self.unit)] for _ in range(self.unit)], dtype=np.uint8))
+        mpimg.imsave("eatCase.png", np.array([[[165, 38, 10, 100] for _ in range(self.unit)] for _ in range(self.unit)], dtype=np.uint8))
+        mpimg.imsave("selectCase.png", np.array([[[1, 215, 88, 80] for _ in range(self.unit)] for _ in range(self.unit)], dtype=np.uint8))
+        mpimg.imsave("castlingCase.png", np.array([[[255, 215, 0, 60] for _ in range(self.unit)] for _ in range(self.unit)], dtype=np.uint8))
 
-            # We clean the repository
-            os.system("rm background.png white\_case.png black\_case.png eat\_case.png movement\_case.png castling\_case.png select\_case.png")
+        # We load annexes and background for futur uses
+        self.background = pygame.image.load("background.png").convert()
+        self.whiteCase = pygame.image.load("whiteCase.png").convert()
+        self.blackCase = pygame.image.load("blackCase.png").convert()
+        self.movementCase = pygame.image.load("movementCase.png").convert()
+        self.eatCase = pygame.image.load("eatCase.png").convert()
+        self.selectCase = pygame.image.load("selectCase.png").convert()
+        self.castlingCase = pygame.image.load("castlingCase.png").convert()
 
-            # We initialize the pygame window
-            pygame.init()
+        self.pieces_skin = {
+            'pawn': {True: pygame.transform.scale(pygame.image.load("pictures/whitePawn.png").convert_alpha(),(int(self.unit * 0.8), int(self.unit * 0.8) )), False: pygame.transform.scale(pygame.image.load("pictures/blackPawn.png").convert_alpha(), (int(self.unit * 0.8), int(self.unit * 0.8) ))},
+            'rook': {True: pygame.transform.scale(pygame.image.load("pictures/whiteRook.png").convert_alpha(), (int(self.unit * 0.8), int(self.unit * 0.8) )), False: pygame.transform.scale(pygame.image.load("pictures/blackRook.png").convert_alpha(), (int(self.unit * 0.8), int(self.unit * 0.8) ))},
+            'knight': {True: pygame.transform.scale(pygame.image.load("pictures/whiteKnight.png").convert_alpha(), (int(self.unit * 0.8), int(self.unit * 0.8) )), False: pygame.transform.scale(pygame.image.load("pictures/blackKnight.png").convert_alpha(), (int(self.unit * 0.8), int(self.unit * 0.8) ))},
+            'bishop': {True: pygame.transform.scale(pygame.image.load("pictures/whiteBishop.png").convert_alpha(), (int(self.unit * 0.8), int(self.unit * 0.8) )), False: pygame.transform.scale(pygame.image.load("pictures/blackBishop.png").convert_alpha(), (int(self.unit * 0.8), int(self.unit * 0.8) ))},
+            'king': {True: pygame.transform.scale(pygame.image.load("pictures/whiteKing.png").convert_alpha(), (int(self.unit * 0.8), int(self.unit * 0.8) )), False: pygame.transform.scale(pygame.image.load("pictures/blackKing.png").convert_alpha(), (int(self.unit * 0.8), int(self.unit * 0.8) ))},
+            'queen': {True: pygame.transform.scale(pygame.image.load("pictures/whiteQueen.png").convert_alpha(), (int(self.unit * 0.8), int(self.unit * 0.8) )), False: pygame.transform.scale(pygame.image.load("pictures/blackQueen.png").convert_alpha(), (int(self.unit * 0.8), int(self.unit * 0.8) ))}}
 
-            # We display the board
-            self.window = pygame.display.set_mode((unit*8, unit*8))
-            self.window.blit(background, (0, 0))
+        # We clean the repository
+        os.system("rm background.png whiteCase.png blackCase.png eatCase.png movementCase.png castlingCase.png selectCase.png")
 
-            # We display the initial board configuration
-            to_update = []
+        # We initialize the pygame window
+        # pygame.init()
 
-            for couleur in self.pieces:
-                to_update += self.pieces[couleur]
+        # We display the board
+        self.window.blit(self.background, (0, 0))
 
-            self.updateDisplay(to_update)
+        pygame.display.flip()
+
+        # We display the initial board configuration
+        to_update = []
+
+        for couleur in self.pieces:
+            to_update += self.pieces[couleur]
+
+        self.updateDisplay(to_update)
 
 
-        def applySlection(self, selectElement, movement, eat, castling, enPassant):
-            fenetre.blit(selectCase, (selectElement[0] *unit, (7-selectElement[0]) * unit))
-            for y, x in movement:
-                fenetre.blit(movemeent_case, (i*unit, (7-j) * unit))
-            for y, x in eat:
-                fenetre.blit(eatCase, (i*unit, (7-j) * unit))
-            for y, x in castling:
-                fenetre.blit(castlingCase, (i*unit, (7-j) * unit))
-            for y, x in enPassant:
-                fenetre.blit(eatCase, (i*unit, (7-j) * unit))
-            pygame.display.flip()
+    def applySelection(self, selectElement, movement, eat, castling, enPassant):
+        self.window.blit(self.selectCase, (selectElement[0] *self.unit, (7-selectElement[0]) * self.unit))
+        for y, x in movement:
+            self.window.blit(self.movement_case, (i*self.unit, (7-j) * self.unit))
+        for y, x in eat:
+            self.window.blit(self.eatCase, (i*self.unit, (7-j) * self.unit))
+        for y, x in castling:
+            self.window.blit(self.castlingCase, (i*self.unit, (7-j) * self.unit))
+        for y, x in enPassant:
+            self.window.blit(self.eatCase, (i*self.unit, (7-j) * self.unit))
+        pygame.display.flip()
 
-        def updateDisplay(self, to_update):
-            for y, x in to_update:
-                self.window.blit(whiteCase if (x+y+1)%2 == 0 else blackCase,
-                (x * unit, (7-y) * unit))
-                if self.map[y][x] != None:
-                    self.window.blit(pieces_skin[self.map[y][x].piecetype][self.map[y][x].couleur],
-                    (int((x+0.1) * unit), int((7-y + 0.1) * unit)))
+    def updateDisplay(self, to_update):
+        for y, x in to_update:
+            self.window.blit(self.whiteCase if (x+y+1)%2 == 0 else self.blackCase,
+            (x * self.unit, (7-y) * self.unit))
+            if self.map[y][x] != None:
+                self.window.blit(self.pieces_skin[self.map[y][x].piecetype][self.map[y][x].color],
+                (int((x+0.1) * self.unit), int((7-y + 0.1) * self.unit)))
 
-            pygame.display.flip()
+        pygame.display.flip()
+
+    def getEvent(self):
+        while True:
+            for event in pygame.event.get(): # We ask the user to do something
+                if event.type == QUIT: # We close the pygame window
+                    pygame.quit()
+                    return False
+                if event.type == MOUSEBUTTONDOWN and event.button == 1: # We return the coordonate of the click
+                    x, y = event.pos
+                    return  7 - y // self.unit, x // self.unit
